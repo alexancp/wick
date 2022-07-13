2292d690cb2b1462556a2c8883cc9ff3bbcdbb02
Alex <alexander.c.paul@ntnu.no>
2022-07-13 09:14:48 +0200

    Added spinadapted examples

--- examples/ccsd_omega1_spinadapted.py
+++ examples/ccsd_omega1_spinadapted.py
@@ -1,7 +1,7 @@
 from fractions import Fraction
 from wick.expression import AExpression
 from wick.wick import apply_wick
-from wick.convenience import commute, one_e_spin, two_e_spin, E2_spin, braE2_spin
+from wick.convenience import commute, one_e_spin, two_e_spin, E2_spin, braE1_spin
 
 o_general = "ijklmno"
 v_general = "abcdefg"
@@ -16,37 +16,28 @@ index_dict = {"o_a": o_alpha, "v_a": v_alpha,
               "o_b": o_beta, "v_b": v_beta,
               "o": o_general, "v": v_general}
 
+T2 = E2_spin("t2", [("o_a", "v_a"), ("o_b", "v_b")], index_key=index_dict)
+# Use T1-transformed Hamiltonian
+T = T2
+
 H1 = one_e_spin("F", [("o_a", "v_a"), ("o_b", "v_b")], norder=True, index_key=index_dict)
 H2 = two_e_spin("g", [("o_a", "v_a"), ("o_b", "v_b")], norder=True, index_key=index_dict)
 
-T2 = E2_spin("t2", [("o_a", "v_a"), ("o_b", "v_b")], index_key=index_dict)
-T = T2
-
-doubles_projection = braE2_spin([("o_a", "v_a"), ("o_b", "v_b")], index_key=index_dict)
-
 H = H1 + H2
-print("[H, T]")
 HT = commute(H, T)
-print("[[H, T], T]")
 HTT = commute(HT, T)
 
-Omega2 = doubles_projection*(H + HT + Fraction('1/2')*HTT)
+singles_projection = braE1_spin(["o_a", "o_b"], ["v_a", "v_b"], index_key=index_dict)
 
-print("Apply wick")
-out = apply_wick(Omega2)
+Omega1 = singles_projection*(H + HT + Fraction('1/2')*HTT)
 
-print("Resolve")
+out = apply_wick(Omega1)
 out.resolve()
-
-print("Omega2 with explicit spin contributions:")
 explicit_spin = AExpression(Ex=out)
 
 space_dict = {"o_a": "o", "o_b": "o", "v_a": "v", "v_b": "v"}
-
 final = explicit_spin.update_index_spaces(space_dict)
 final.simplify()
-print("\nOmega2 spin-adapted expression:")
-print(final)
 
-print("\nIntroduce permutation operators:")
-print(final.introduce_permutation_operators((2, 3, 0, 1)))
+print("Omega1 spin-adapted expression:")
+print(final)
--- examples/ccsd_omega2_spinadapted.py
+++ examples/ccsd_omega2_spinadapted.py
@@ -1,7 +1,7 @@
 from fractions import Fraction
 from wick.expression import AExpression
 from wick.wick import apply_wick
-from wick.convenience import commute, one_e_spin, two_e_spin, E2_spin, braE1_spin
+from wick.convenience import commute, one_e_spin, two_e_spin, E2_spin, braE2_spin
 
 o_general = "ijklmno"
 v_general = "abcdefg"
@@ -16,35 +16,29 @@ index_dict = {"o_a": o_alpha, "v_a": v_alpha,
               "o_b": o_beta, "v_b": v_beta,
               "o": o_general, "v": v_general}
 
-H1 = one_e_spin("F", [("o_a", "v_a"), ("o_b", "v_b")], norder=True, index_key=index_dict)
-H2 = two_e_spin("g", [("o_a", "v_a"), ("o_b", "v_b")], norder=True, index_key=index_dict)
-
 T2 = E2_spin("t2", [("o_a", "v_a"), ("o_b", "v_b")], index_key=index_dict)
 T = T2
 
-singles_projection = braE1_spin(["o_a", "o_b"], ["v_a", "v_b"], index_key=index_dict)
+H1 = one_e_spin("F", [("o_a", "v_a"), ("o_b", "v_b")], norder=True, index_key=index_dict)
+H2 = two_e_spin("g", [("o_a", "v_a"), ("o_b", "v_b")], norder=True, index_key=index_dict)
 
 H = H1 + H2
-print("[H, T]")
 HT = commute(H, T)
-print("[[H, T], T]")
 HTT = commute(HT, T)
 
-Omega1 = singles_projection*(H + HT + Fraction('1/2')*HTT)
+doubles_projection = braE2_spin([("o_a", "v_a"), ("o_b", "v_b")], index_key=index_dict)
+Omega2 = doubles_projection*(H + HT + Fraction('1/2')*HTT)
 
-print("\nApply wick")
-out = apply_wick(Omega1)
-
-print("\nResolve")
+out = apply_wick(Omega2)
 out.resolve()
-
-print("\nOmega1 with explicit spin contributions:")
 explicit_spin = AExpression(Ex=out)
-print(explicit_spin)
 
 space_dict = {"o_a": "o", "o_b": "o", "v_a": "v", "v_b": "v"}
 
 final = explicit_spin.update_index_spaces(space_dict)
 final.simplify()
-print("\nOmega1 spin-adapted expression:")
+print("Omega2 spin-adapted expression:")
 print(final)
+
+print("\nIntroduce permutation operators:")
+print(final.introduce_permutation_operators((2, 3, 0, 1)))
